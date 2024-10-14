55ccde32110753e35973efac8b2fa5023a30a951
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-10-14 10:47:57 +0530

    add site to allowed hosts

--- CodeGenTools/settings.py
+++ CodeGenTools/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",") if os.environ.get("ALLOWED_HOSTS") else []
+ALLOWED_HOSTS = ['codegentools.pythonanywhere.com'] if not DEBUG else []
 
 # Application definition
 
